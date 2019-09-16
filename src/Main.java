857717eb14146b9a22ff5c0f58a0b1fc817f5d43
Azat <galimov-azat@bk.ru>
2019-09-16 20:01:50 +0300

    Slight changes.

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Room length in meters: "); // Длина комнаты.
+        System.out.print("Room length in meters: ");
         double roomLength = in.nextDouble();
-        System.out.print("Room Width in meters : "); // Ширина комнаты.
+        System.out.print("Room Width in meters : ");
         double roomWidth = in.nextDouble();
-        System.out.print("Room height in meters: "); // Высота комнаты.
+        System.out.print("Room height in meters: ");
         double roomHeight = in.nextDouble();
-        System.out.print("Wallpaper width in meters : "); // Ширина обоев.
+        System.out.print("Wallpaper width in meters : ");
         double wallpaperWidth = in.nextDouble();
-        System.out.print("Wallpaper length in meters: "); // Длина обоев.
+        System.out.print("Wallpaper length in meters: ");
         double wallpaperLength = in.nextDouble();
-        double wallpaperStoke = 0.1; // Запас обоев.
-        double wallpaperSheet = Math.floor(wallpaperLength /(roomHeight + wallpaperStoke)); // Количество полотниц с одного рулона.
-        double perimeter = (roomLength * 2) + (roomWidth * 2); //Периметр комнаты.
-        double leaf = Math.ceil(perimeter / wallpaperWidth); //Необходимов количество полотниц.
+        double wallpaperStoke = 0.1; //
+        double wallpaperSheet = Math.floor(wallpaperLength /(roomHeight + wallpaperStoke));
+        double perimeter = (roomLength * 2) + (roomWidth * 2);
+        double leaf = Math.ceil(perimeter / wallpaperWidth);
         double rollOfWallpaper = (leaf / wallpaperSheet);
-        System.out.println ("You will need " + Math.ceil(rollOfWallpaper) + " wallpaper rolls."); //Необходимое количество рулонов.
+        System.out.println ("Will need " + Math.ceil(rollOfWallpaper) + " wallpaper rolls.");
         in.close();
     }
 }
